3633222403bb00821fba18f9bfe91d12372d0972
Pavan Edirisingha <96134390+PavanEdirisingha2000@users.noreply.github.com>
2022-02-22 02:08:44 +0530

    update

--- web.php
+++ web.php
@@ -55,13 +55,13 @@
         return false;
       }
 
-      if(psw != pswrepeat)
+      if(q != r)
       {
         alert("Password don't match");
         return false;
       }
 
-      let s = document.forms["myform"]["Seller/Buyer"];
+      let s = document.forms["myform"]["identity"];
       if (((s[0].checked == false) && (s[1].checked == false))) {
         alert("Please Select whether you want to be a seller or a buyer");
         return false;
@@ -77,18 +77,18 @@
 <body>
   
 
-<form action="signup.php" onsubmit="return validateForm()" method ="post" style="border:1px solid #ccc">
+<form name="myform" onsubmit="return validateForm()" action="signup.php" method ="POST" style="border:1px solid #ccc">
   <div class="container">
     <center>
     <h1>Sign Up</h1>
-    <p>You already have an account Please <a href="log in.php" style="color:rgb(1, 175, 166)">log in</a> here</p>
+    <p>You already have an account Please <a href="log_in.php" style="color:rgb(1, 175, 166)">log in</a> here</p>
     <hr>
     <label for="name"></label>
     <input type="text" placeholder="Enter Name" name="name" >
   </br>
 
   <label for="email"></label>
-    <input type="text" placeholder="Enter Email" name="email">
+    <input type="text" placeholder="Enter Email" name="email" >
   </br>
     
 
@@ -97,7 +97,7 @@
   </br>
 
   <label for="contactNo"></label>
-    <input type="text" placeholder="Contact Number" name="contactNo" pattern="[0]{1}[7]{1}[0-9]{8}" required>
+    <input type="text" placeholder="Contact Number" name="contactNo" pattern="[0]{1}[7]{1}[0-9]{8}" >
   </br>
 
   <label for="psw"></label>
@@ -114,15 +114,46 @@ Seller
     <input type="radio" name="identity" value="1" >
     Buyer 
     <input type="radio" name="identity" value="2" >
-</br></br>
+</br>
     
+<h2>Choose what you hope to Sell or buy.</h2>
+
+<label class="scontainer">Ploythene
+  <input type="checkbox" name="Ploythene" value="p">
+  <span class="checkmark"></span>
+</label>
+<label class="scontainer">Glass
+  <input type="checkbox" name="Glass" value="g">
+  <span class="checkmark"></span>
+</label>
+<label class="scontainer">Coconut Huskd
+  <input type="checkbox" name="Husked" value="h">
+  <span class="checkmark"></span>
+</label>
+
+<label class="scontainer">Usead book and Papers
+  <input type="checkbox" name="books" value="b">
+  <span class="checkmark"></span>
+</label>
+<label class="scontainer">Usead vehicle parts
+  <input type="checkbox" name="vehicle" value="v">
+  <span class="checkmark"></span>
+</label>
+<label class="scontainer">Usead electronic divices
+  <input type="checkbox" name="electronic" value="e">
+  <span class="checkmark"></span>
+</label>
+  
+
+
+
     
     <p>By creating an account you agree to our Terms & Privacy</a>.</p>
     
 
     <div class="clearfix">
       
-      <button type="submit" class="signupbtn">Sign Up</button>
+      <button type="submit" name="submit" class="signupbtn">Sign Up</button>
     </center>
     </div>
   </div>
